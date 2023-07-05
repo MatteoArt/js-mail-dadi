cc223ebb4a5fe3b817239dfe1c101762ee6a5bbb
MatteoArt <mattrim8@gmail.com>
2023-07-05 18:16:23 +0200

    finisco es mail

--- jsmail/main2.js
+++ jsmail/main2.js
@@ -1,5 +1,36 @@
+const class1 = "success";
+const class2 = "fail";
+
 //array contenente ipotetiche mail fittizie
 const mail_arr = ["johndoe@example.com","marcobianchi@gmail.com",
 "federicorossi@example.com","giopizzi@aruba.it","sergioforti@dnet.com",
 "tizioacaso@example.it","dmitrivlador@io.com","miamail@tiscali.it",
 "secondamail@tiscali.it"];
+
+//recupero l'input dell'utente e il button
+const userMailElement = document.querySelector(".input-mail");
+const btnElement = document.querySelector(".btn");
+
+const outputElement = document.querySelector(".output");
+
+//al click sul button 
+btnElement.addEventListener("click", function () {
+    let mail = userMailElement.value; //recupero la mail inserita
+    //console.log(mail);
+
+    let mail_trovata = false;
+
+    for (let i = 0; i < mail_arr.length; i++) {
+        let match = mail_arr[i];
+
+        if (match === mail) {
+            mail_trovata = true;
+        }
+    }
+
+    if (mail_trovata === true) {
+        outputElement.innerHTML = `<span class='${class1}'>Accesso consentito: la tua mail è registrata</span>`;
+    } else {
+        outputElement.innerHTML = `<span class='${class2}'>Accesso negato: la mail non è registrata</span>`;
+    }
+});
