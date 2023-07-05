61a6153230341a4510013da30ed09ad9128a85db
MatteoArt <mattrim8@gmail.com>
2023-07-05 17:32:13 +0200

    imposto esercizio

---
+++ jsmail/main2.js
@@ -0,0 +1,5 @@
+//array contenente ipotetiche mail fittizie
+const mail_arr = ["johndoe@example.com","marcobianchi@gmail.com",
+"federicorossi@example.com","giopizzi@aruba.it","sergioforti@dnet.com",
+"tizioacaso@example.it","dmitrivlador@io.com","miamail@tiscali.it",
+"secondamail@tiscali.it"];
